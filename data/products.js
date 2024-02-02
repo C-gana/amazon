40c27bf5e1bad8c832d4faf700cada67112d66d4
Cgana <patiencebanda19@gmail.com>
2024-02-02 14:40:07 +0200

    using amazon.js to generate HTML

--- data/products.js
+++ data/products.js
@@ -1,5 +1,25 @@
 const products = [{
+    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+    rating: {
+        stars: 4.5,
+        voters: 87
+    },
+    priceCents: 1090
+}, {
+    image: 'images/products/intermediate-composite-basketball.jpg',
+    name: 'Intermediate Size Basketball',
+    rating: {
+        stars: 4.0,
+        voters: 127
+    },
+    priceCents: 2095
+}, {
     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
     name: 'Adult Plain Cotton T-shirt - 2 Pack',
+    rating: {
+        stars: 4.5,
+        voters: 56
+    },
     priceCents: 799
 }]
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,31 +2,37 @@ let productsHTML = '';
 
 products.forEach((product) => {
     const html = `
-  <div class="product">
-  <img
-    class="product-image js-product-image"
-    src="${product.image}"
-    alt=""
-  />
-  <div class="product-name js-product-name">
-    ${product.name}
+    <div class="product">
+    <div class="product-image-container">
+      <img
+        class="product-image js-product-image"
+        src="${product.image}"
+        alt=""
+      />
+    </div>
+    <div class="product-name js-product-name">
+      ${product.name}
+    </div>
+    <div class="rating">
+      <img class="stars" src="images/ratings/rating-${product.rating.stars *10}.png" alt="" />
+      <div class="voters">${product.rating.voters}</div>
+    </div>
+    <div class="price">$<span class="js-price">${(product.priceCents/100).toFixed(2)}</span></div>
+    <select class="js-quantity" name="quantity" id="">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+    </select>
+    <div class="js-added-confirmation added-confirmation"></div>
+    <button class="js-add-button add-button">Add to Cart</button>
   </div>
-  <div class="price">$<span class="js-price">${(product.priceCents/100).toFixed(2)}</span></div>
-  <select class="js-quantity" name="quantity" id="">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-    <option value="9">9</option>
-    <option value="10">10</option>
-  </select>
-  <div class="js-added-confirmation added-confirmation"></div>
-  <button class="js-add-button add-button">Add to Cart</button>
-</div>
   `;
 
     productsHTML += html;
