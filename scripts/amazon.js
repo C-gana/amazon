6891b17eeb71ec60a5f5d457fe26d46c6fb1c282
Cgana <patiencebanda19@gmail.com>
2024-02-09 15:27:24 +0200

    adding items to the cart but not updating the DOM cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
-let productsHTML = '';
+let productsHTML = "";
 
 products.forEach((product) => {
-    const html = `
+  const html = `
     <div class="product">
     <div class="product-image-container">
       <img
@@ -14,10 +14,14 @@ products.forEach((product) => {
       ${product.name}
     </div>
     <div class="rating">
-      <img class="stars" src="images/ratings/rating-${product.rating.stars *10}.png" alt="" />
+      <img class="stars" src="images/ratings/rating-${
+        product.rating.stars * 10
+      }.png" alt="" />
       <div class="voters">${product.rating.count}</div>
     </div>
-    <div class="price">$<span class="js-price">${(product.priceCents/100).toFixed(2)}</span></div>
+    <div class="price">$<span class="js-price">${(
+      product.priceCents / 100
+    ).toFixed(2)}</span></div>
     <select class="js-quantity" name="quantity" id="">
       <option value="1">1</option>
       <option value="2">2</option>
@@ -31,11 +35,41 @@ products.forEach((product) => {
       <option value="10">10</option>
     </select>
     <div class="js-added-confirmation added-confirmation"></div>
-    <button class="js-add-button add-button">Add to Cart</button>
+    <button class="js-add-button add-button"
+      data-product-id="${product.id}"
+    >Add to Cart</button>
   </div>
   `;
 
-    productsHTML += html;
+  productsHTML += html;
 });
 
-document.querySelector('.js-products').innerHTML = productsHTML;
+document.querySelector(".js-products").innerHTML = productsHTML;
+
+//making the add button responsive and adding products to cart
+document.querySelectorAll(".js-add-button").forEach((button, i) => {
+  button.addEventListener("click", () => {
+    //getting the product id from the dataset attribute
+    const productId = button.dataset.productId;
+    const quantityElement = document.querySelectorAll(".js-quantity");
+    const quantity = Number(quantityElement[i].value);
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: quantity,
+      });
+    }
+
+    console.log(cart);
+  });
+});
--- scripts/cart.js
+++ scripts/cart.js
@@ -1,6 +1,1 @@
-document.querySelectorAll('.js-add-button')
-    .forEach((button) => {
-        button.addEventListener('click', () => {
-            console.log('ok');
-        })
-    })
+let cart = [];
